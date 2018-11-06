d58bdc10218784ab9c5853aba472989e10a96e1e
Hemanth <hemanth@Hemanths-MacBook-Pro.local>
2018-11-06 17:51:05 +0530

    fixed broken database link

--- tournament/views.py
+++ tournament/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from . import forms
 # Create your views here.
 
 
-
+        
 def tournament(request):
-    form = forms.Enroll()
-    if request.method == "POST":
-        form = forms.Enroll(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-    return render(request, 'home.html',{'Form':form})
+        form = forms.Enroll()
+        return render(request, 'home.html',{'Form':form})
 
 def paytm(request):
+        form = forms.Enroll()
+        if request.method == "POST":
+                form = forms.Enroll(request.POST)
+                if form.is_valid():
+                        form.save(commit=True)
         return HttpResponseRedirect('http://m.p-y.tm/requestPayment?recipient=7075755633&amount=200&comment=squardtournament')
+
+# def paytm(request):
+#         return HttpResponseRedirect('')
